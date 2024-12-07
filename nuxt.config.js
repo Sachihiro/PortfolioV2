610b16ffea35eed3e8655ee567333e6718692e1d
Rex <rexroys@gmail.com>
2024-12-07 20:41:08 +0800

    :+1: add static pages to be deploy in github pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,5 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   builder: 'vite', // Ensure Vite is set as the builder
-  baseURL: '/PortfolioV2/'
+  baseURL: '/PortfolioV2/',
+  generate: {
+    // Optional: customize if needed
+    dir: 'dist', // This should be the default
+  },
 })
