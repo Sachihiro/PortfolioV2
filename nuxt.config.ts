dcefed2fe72c388d78b3d733446b05f4d33ae768
Rex <rexroys@gmail.com>
2024-12-07 20:22:41 +0800

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  builder: 'vite', // Ensure Vite is set as the builder
+})
